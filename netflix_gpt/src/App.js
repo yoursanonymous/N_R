fbb40b7f52a4f194060a7a17d0a0f826c8de31ed
yoursanonymous <vinayaks0111@gmail.com>
2025-01-29 12:34:23 +0530

    starting a new project netflix-gpt

---
+++ netflix_gpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-500">
+      namaste every
+    </div>
+  );
+}
+
+export default App;
